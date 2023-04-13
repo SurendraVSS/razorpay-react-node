d9f139512e7bcaac61209113f0944ae23fe581fc
SurendraVSS <106951672+SurendraVSS@users.noreply.github.com>
2023-04-13 15:11:25 +0530

    razorpay

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -3,17 +3,22 @@ import crypto from "crypto";
 import { Payment } from "../models/paymentModel.js";
 
 export const checkout = async (req, res) => {
-  const options = {
-    amount: Number(req.body.amount * 100),
-    currency: "INR",
-  };
-  const order = await instance.orders.create(options);
+  try{
+    const options = {
+      amount: Number(req.body.amount * 100),
+      currency: "INR",
+    };
+    const order = await instance.orders.create(options);
 
-  res.status(200).json({
-    success: true,
-    order,
-  });
-};
+    res.status(200).json({
+      success: true,
+      order,
+    });
+  } catch (err) {
+  console.log(err);
+}
+
+}
 
 export const paymentVerification = async (req, res) => {
   const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
